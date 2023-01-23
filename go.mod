09a7f666912d446a1720515de0dcdceda595745f
TomislavGalic <tomislav.galic19@gmail.com>
2023-01-23 15:34:29 +0100

    initializers, models, database

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.4.0
 	gorm.io/gorm v1.24.3
 )
 
@@ -12,6 +13,7 @@ require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.2.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
 	golang.org/x/crypto v0.4.0 // indirect
 	golang.org/x/text v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,111 +1,75 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/TomislavGalic/CRUDAPI/initializers"
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
 
-/*
-type Model struct {
-	Model_id              int    `json:"id"`
-	Model_name            string `json:"model_name"`
-	First_production_year int    `json:"first_production_year"`
+func init() {
+	initializers.LoadEnv()
+	initializers.ConnectToDatabase()
 }
-
-type Make struct {
-	Make_id        int    `json:"id"`
-	Make_name string `json:"make_name"`
-	Country   string `json:"country"`
-}
-
-type Inventory struct {
-	Inventory_id         int     `json:"id"`
-	Vehicle_id int     `json:"vehicle_id"`
-	Color_id   int     `json:"color_id"`
-	Price      float64 `json:"price"`
-}
-
-type Color struct {
-	Color_id   int    `json:"id"`
-	Name string `json:"name"`
-	Code int    `json:"code"`
-}
-*/
-
-var DB *gorm.DB
-var err error
 
 func main() {
 
-	dbURI := "host=localhost user=postgres password=tomis dbname=vehicles port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-
-	DB, err = gorm.Open(postgres.Open(dbURI), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println("Successfully connected to the database")
-	}
-
-	DB.AutoMigrate(&Vehicle{})
-
 	r := mux.NewRouter()
-	r.HandleFunc("/vehicles", getVehicles).Methods("GET")
-	r.HandleFunc("/createvehicles", createVehicles).Methods("POST")
-	r.HandleFunc("/getvehicle/{id}", getVehicle).Methods("GET")
-	r.HandleFunc("/updatevehicles/{id}", updateVehicles).Methods("PUT")
-	r.HandleFunc("/deletevehicles/{id}", deleteVehicles).Methods("DELETE")
+	/*	r.HandleFunc("/vehicles", GetVehicles).Methods("GET")
+		r.HandleFunc("/createvehicles", CreateVehicles).Methods("POST")
+		r.HandleFunc("/getvehicle/{id}", GetVehicle).Methods("GET")
+		r.HandleFunc("/updatevehicles/{id}", UpdateVehicles).Methods("PUT")
+		r.HandleFunc("/deletevehicles/{id}", DeleteVehicles).Methods("DELETE")
+	*/
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
-func getVehicles(w http.ResponseWriter, r *http.Request) {
+/*
+func GetVehicles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-	var vehicle []Vehicle
+	var vehicle []models.Vehicle
 	DB.Find(&vehicle)
 	json.NewEncoder(w).Encode(vehicle)
 
 	json.NewEncoder(w).Encode(&vehicle)
 }
 
-func getVehicle(w http.ResponseWriter, r *http.Request) {
+func GetVehicle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	params := mux.Vars(r)
-	var vehicle Vehicle
+	var vehicle models.Vehicle
 	DB.First(&vehicle, params["id"])
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	DB.Save(&vehicle)
 	json.NewEncoder(w).Encode(vehicle)
 }
 
-func createVehicles(w http.ResponseWriter, r *http.Request) {
+func CreateVehicles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
-	var vehicle Vehicle
+	var vehicle models.Vehicle
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	DB.Create(&vehicle)
 	json.NewEncoder(w).Encode(vehicle)
 }
 
-func updateVehicles(w http.ResponseWriter, r *http.Request) {
+func UpdateVehicles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	params := mux.Vars(r)
-	var vehicle Vehicle
+	var vehicle models.Vehicle
 	DB.First(&vehicle, params["id"])
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	DB.Save(&vehicle)
 	json.NewEncoder(w).Encode(vehicle)
 }
 
-func deleteVehicles(w http.ResponseWriter, r *http.Request) {
+func DeleteVehicles(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-type", "application/json")
 	params := mux.Vars(r)
-	var vehicle Vehicle
+	var vehicle models.Vehicle
 	json.NewDecoder(r.Body).Decode(&vehicle)
 	DB.Delete(&vehicle, params["id"])
 	json.NewEncoder(w).Encode("The user is deleted")
 }
+*/
