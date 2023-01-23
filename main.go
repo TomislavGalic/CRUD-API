4216de7ef27531b4b1cd0c91d85c0ca1ab1ce48f
TomislavGalic <tomislav.galic19@gmail.com>
2023-01-23 13:03:43 +0100

    models

--- main.go
+++ main.go
@@ -12,13 +12,6 @@ import (
 	"gorm.io/gorm"
 )
 
-type Vehicle struct {
-	gorm.Model
-
-	Make_id  string `json:"make_id"`
-	Model_id string `json:"model_id"`
-}
-
 /*
 type Model struct {
 	Model_id              int    `json:"id"`
