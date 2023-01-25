82742849da865b5efa490c723e4e3f1f555d9d58
TomislavGalic <tomislav.galic19@gmail.com>
2023-01-25 17:26:30 +0100

    dockerfile

--- models/models.go
+++ models/models.go
@@ -5,31 +5,6 @@ import "gorm.io/gorm"
 type Vehicle struct {
 	gorm.Model
 
-	Make_id  string `json:"make_id"`
-	Model_id string `json:"model_id"`
-}
-
-type Model struct {
-	Model_id              int    `json:"id"`
-	Model_name            string `json:"model_name"`
-	First_production_year int    `json:"first_production_year"`
-}
-
-type Make struct {
-	Make_id   int    `json:"id"`
-	Make_name string `json:"make_name"`
-	Country   string `json:"country"`
-}
-
-type Inventory struct {
-	Inventory_id int     `json:"id"`
-	Vehicle_id   int     `json:"vehicle_id"`
-	Color_id     int     `json:"color_id"`
-	Price        float64 `json:"price"`
-}
-
-type Color struct {
-	Color_id int    `json:"id"`
-	Name     string `json:"name"`
-	Code     int    `json:"code"`
+	Make_name  string `json:"make_name"`
+	Model_name string `json:"model_name"`
 }
